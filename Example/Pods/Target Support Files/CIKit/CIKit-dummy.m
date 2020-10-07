9df14e9a54b82d7071371b0378d139a99394db45
Exquisitian <Exquisitian@gmail.com>
2020-10-06 19:17:03 -0700

    Init Cocoapods Template

---
+++ Example/Pods/Target Support Files/CIKit/CIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CIKit : NSObject
+@end
+@implementation PodsDummy_CIKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-CIKit_Tests/Pods-CIKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CIKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CIKit_Tests
+@end
